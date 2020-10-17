661502432ecff8f642f74a810e1053fe25ecf70b
cjy324 <juy32400@gmail.com>
2020-10-17 17:19:04 +0900

    20.10.16 코딩연습(보충)(if문 최소화, article 배열로 정리)

--- src/Practice/App.java
+++ src/Practice/App.java
@@ -3,117 +3,95 @@ package Practice;
 import java.util.Scanner;
 
 public class App {
-	
-	// 1번 게시물 저장소
-	Article article1 = new Article();
 
-	// 2번 게시물 저장소
-	Article article2 = new Article();
-	
-	public Article getArticle(int id) { // getArticle을 통해 해당하는 id의 article 정보를 받아오기
-		if(id == 1) {
-			return article1;			
+	Article[] articles = new Article[10];   // articles라는 이름의 Article배열 10칸을 만듬
+
+	public Article getArticle(int id) {
+		if (id > articles.length) {
+			return null;
 		}
-		if(id == 2) {
-			return article2;			
-		}
-		return null;  // null값을 반드시 리턴해 주어야 id가 1,2가 아닌 경우에도 null값으로 리턴하여 정상작동 가능
+		return articles[id - 1];   // 만약 1번 게시물 틀을 불러오고 싶으면 [0]번째 칸에 있는 정보를 가져와야하므로 id-1
+
 	}
-	
 
-	public void run() {
-		// TODO Auto-generated method stub
-		
-		Scanner scanner = new Scanner(System.in);
+	public void run() { // class 필드에선 변수 선언까지만 가능
+						// 기능(메소드) 구현을 위해 void 필드에 기능 구현
+//		articles[0]=article1;    //articles 배열 첫번째 칸에 article1을 채워넣음
+
+		for (int i = 0; i < articles.length; i++) { // articles 배열 i번째 칸에 Article객체를 채워넣는 것을
+													// articles.length(배열의 길이)까지 반복
+			articles[i] = new Article(); // 만약 articles.length가 100이면 i는 99까지 증가하지만
+											// []칸은 100칸이 채워지는 것임
+		}
+
+		Scanner sc = new Scanner(System.in);
 
 		int lastArticleId = 0;
 
 		while (true) {
 			System.out.printf("명령어) ");
-			String command = scanner.nextLine();
+			String command = sc.nextLine();
 
-			if (command.startsWith("article detail ")) {
-				int inputedId = Integer.parseInt(command.split(" ")[2]);
-
-				System.out.println("== 게시물 상세 ==");
-
-				if (inputedId == 1) {
-					if (article1.id == 0) {
-						System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedId);
-						continue;
-					}
-
-					System.out.printf("번호 : %s\n", article1.id);
-					System.out.printf("제목 : %s\n", article1.title);
-					System.out.printf("내용 : %s\n", article1.body);
-				} else if (inputedId == 2) {
-					if (article2.id == 0) {
-						System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedId);
-						continue;
-					}
-
-					System.out.printf("번호 : %s\n", article2.id);
-					System.out.printf("제목 : %s\n", article2.title);
-					System.out.printf("내용 : %s\n", article2.body);
-				} else {
-					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedId);
-				}
-
-			} else if (command.equals("article list")) {
-				System.out.println("== 게시물 리스트 ==");
-
-				if (lastArticleId == 0) {
-					System.out.println("게시물이 없습니다.");
-					continue;
-				}
-
-				System.out.println("번호 / 제목");
-
-				if (lastArticleId >= 1) {
-					System.out.printf("%d / %s\n", article1.id, article1.title);
-				}
-
-				if (lastArticleId >= 2) {
-					System.out.printf("%d / %s\n", article2.id, article2.title);
-				}
-			} else if (command.equals("article add")) {
+			if (command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
 
 				int id = lastArticleId + 1;
 				String title;
 				String body;
 
-				System.out.printf("제목 : ");
-				title = scanner.nextLine();
-				System.out.printf("내용 : ");
-				body = scanner.nextLine();
+				lastArticleId = id;
+				System.out.printf("제목 입력: ");
+				title = sc.nextLine();
+				System.out.printf("내용 입력: ");
+				body = sc.nextLine();
+				System.out.printf("%d번글이 생성되었습니다.\n", id);
 
-				if (id == 1) {
-					article1.id = id;
-					article1.title = title;
-					article1.body = body;
-				} else if (id == 2) {
-					article2.id = id;
-					article2.title = title;
-					article2.body = body;
+				Article article = getArticle(id);
+
+				article.id = id;
+				article.title = title;
+				article.body = body;
+
+			}
+
+			if (command.equals("article list")) {
+				if (lastArticleId == 0) {
+					System.out.println("게시물이 존재하지 않습니다.");
+					continue;
 				}
 
-				System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
+				System.out.println("== 게시물 리스트 ==");
+				System.out.println("번호 / 제목");
 
-				// 가장 마지막 게시물 번호를 갱신한다.
-				// 왜냐하면 방금 새 게시물이 생성되었기 때문.
-				lastArticleId = id;
-			} else if (command.equals("system exit")) {
+				for (int i = 1; i <= lastArticleId; i++) {
+					Article article = getArticle(i);
+					System.out.printf("%d / %s\n", article.id, article.title);
+				}
+			}
+			if (command.startsWith("article detail ")) {
+				int inputedId = Integer.parseInt(command.split(" ")[2]);
+				System.out.println("== 게시물 상세 ==");
+
+				Article selectedArticle = getArticle(inputedId);
+
+				if (selectedArticle == null || selectedArticle.id == 0) {
+					System.out.printf("%d번 글이 존재하지 않습니다.\n", inputedId);
+					continue;
+				}
+				System.out.printf("번호 : %d\n", selectedArticle.id);
+				System.out.printf("제목 : %s\n", selectedArticle.title);
+				System.out.printf("내용 : %s\n", selectedArticle.body);
+
+			}
+
+			else if (command.equals("exit")) {
 				System.out.println("== 프로그램 종료 ==");
 				break;
 			}
 
 		}
+		sc.close();
 
-		scanner.close();
-
-		
-		
 	}
 
 }
--- src/Practice/Article.java
+++ src/Practice/Article.java
@@ -5,4 +5,5 @@ public class Article {
 	public int id;
 	public String title;
 	public String body;
+	
 }
