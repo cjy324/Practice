e48daf8acbd42c307383fdf0400ca61b3c44f21d
cjy324 <juy32400@gmail.com>
2020-10-17 19:50:37 +0900

    test 추가

---
+++ src/Practice/test.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		int 숫자 = 1;
+		String 문자 = "1";
+
+		int number = Integer.parseInt(문자);
+		
+		System.out.println(숫자);
+		System.out.println(number);
+	}
+
+}
