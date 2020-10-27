35cb478b30fd906debb5fd601b3d7bcbdcd99f24
cjy324 <juy32400@gmail.com>
2020-10-27 16:54:08 +0900

    20.10.27 코딩연습(Memeber 회원가입,로그인,로그인회원정보 조회, 로그아웃 그리고 try,catch 적용)

--- src/ArrayListPractice/Article.java
+++ src/ArrayListPractice/Article.java
@@ -1,19 +1,27 @@
 package ArrayListPractice;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Article {
 
 	public int id;
 	public String title;
 	public String body;
-	
+	public String regDate;
 	
 	public Article(int id, String title, String body) {
+	
 		this.id = id;
 		this.title = title;
 		this.body = body;
+
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd / HH:mm:ss");
+		
+		this.regDate = sdf.format(cal.getTime());
+		
 		
 	}
-	
-	
 	
 }
